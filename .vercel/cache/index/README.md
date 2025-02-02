93431ff6505dd58fea8e71a95b5253f872cdcd6a
User <user@example.com>
2025-02-02 06:30:32 +0100

    Nettoyage des fichiers Vercel et optimisation pour déploiement VPS

--- .vercel/cache/index/README.md
+++
@@ -1,2 +0,0 @@
-# website-contact
-scrap website emails and phones
--- .vercel/cache/index/utils/user_agent.py
+++
@@ -1,6 +0,0 @@
-def get_user_agent_headers():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-        
-    }
-    return headers
